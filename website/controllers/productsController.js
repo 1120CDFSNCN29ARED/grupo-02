06dcba0dd9d10b7530cd62e0e6f30607e38f14bd
Santiago Maldini <santimaldini@gmail.com>
2021-02-09 18:06:13 -0300

    Created head, header and vehicleDatasheet partials. Included head and header from productDetails sending params to the head.

--- website/controllers/productsController.js
+++ website/controllers/productsController.js
@@ -1,6 +1,10 @@
 const productsController = {
     details: (req, res) => {
-        res.render("productDetails",{productType: req.params.productType, productID: req.params.productID});
+        res.render("productDetails",
+        {
+            productType: req.params.productType,
+            productID: parseInt(req.params.productID, 10)
+        });
     }
 
 }
