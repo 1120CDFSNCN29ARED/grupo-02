5421907c4faffece31c6070a8a01184a440ee925
Simon Huxter <simon.huxter@gmail.com>
2021-03-04 18:49:49 -0300

    pre merge commit

--- website/models/User.js
+++ website/models/User.js
@@ -1,14 +1,11 @@
 
-//4. Editar la información de un Usuario
-//5. Eliminar a un usuario de la DB
-
-//CRUD
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 const User = {
 	fileName: "./json/users.json",
-	getData: function () {
-		return JSON.parse(fs.readFileSync(this.fileName, "utf-8"));
+  getData: function () {
+    return JSON.parse(fs.readFileSync(this.fileName, "utf-8"));    
 	},
 	writeData: function (userArray) {
 		fs.writeFileSync(this.fileName, JSON.stringify(userArray, null, "  "));
@@ -38,9 +35,10 @@ const User = {
     return newUser;
   },
   generateId: function () {
-    let users = this.findAll();    
+    let users = this.findAll();
     let newId = users.length > 0 ? users[users.length - 1].userID + 1 : 1;
     return newId;
+    //return uuidv4();
   },
   delete: function (id) {
     let users = this.findAll();
