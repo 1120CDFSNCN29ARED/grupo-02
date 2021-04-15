0b780a8336d4575c9bd00199082ef809b112d732
Santiago Maldini <santimaldini@gmail.com>
2021-04-15 01:07:12 -0300

    updated versionsController

--- website/controllers/api/versionsController.js
+++ website/controllers/api/versionsController.js
@@ -3,20 +3,99 @@ const versionsService = require("../../services/versionsService");
 const versionsController = {
     all: async (req, res) => {
         const versions = await versionsService.findAll();
-        return res.status(200).json(versions);
+        const result = {
+            meta: {
+                url: req.originalUrl
+            }
+        };
+        if(versions){
+            result.data = {
+                versions
+            }
+            result.meta.status = 200;
+            result.meta.count = versions.length;
+        }
+        else{
+            result.meta.status = 409;
+            result.meta.count = 0;
+            result.error= {
+                status: "409",
+                message: `No versions were found`
+            }
+        }
+        return res.status(result.meta.status).json(result);
     },
     byID: async (req, res) => {
-        console.log(req.params.versionID)
         const version = await versionsService.findByPk(req.params.versionID);
-        return res.status(200).json(version);
+        const result = {
+            meta: {
+                url: req.originalUrl
+            }
+        };
+        if(version){
+            result.data = {
+                version
+            }
+            result.meta.status = 200;
+            result.meta.count = 1;
+        }
+        else{
+            result.meta.status = 409;
+            result.meta.count = 0;
+            result.error= {
+                status: "409",
+                message: `No versions were found`
+            }
+        }
+        return res.status(result.meta.status).json(result);
     },
     byBrandID: async (req, res) => {
         const versions = await versionsService.findByBrandID(req.params.brandID);
-        return res.status(200).json(versions);
+        const result = {
+            meta: {
+                url: req.originalUrl
+            }
+        };
+        if(versions){
+            result.data = {
+                versions
+            }
+            result.meta.status = 200;
+            result.meta.count = versions.length;
+        }
+        else{
+            result.meta.status = 409;
+            result.meta.count = 0;
+            result.error= {
+                status: "409",
+                message: `No versions were found`
+            }
+        }
+        return res.status(result.meta.status).json(result);
     },
     byModelID: async (req, res) => {
         const versions = await versionsService.findByModelID(req.params.modelID);
-        return res.status(200).json(versions);
+        const result = {
+            meta: {
+                url: req.originalUrl
+            }
+        };
+        if(versions){
+            result.data = {
+                versions
+            }
+            result.meta.status = 200;
+            result.meta.count = versions.length;
+        }
+        else{
+            result.meta.status = 409;
+            result.meta.count = 0;
+            result.error= {
+                status: "409",
+                message: `No versions were found`
+            }
+        }
+        return res.status(result.meta.status).json(result);
     },
     create: async (req, res) => {
         const newData = {
@@ -24,8 +103,28 @@ const versionsController = {
             brandID: req.body.brandID,
             modelID: req.body.modelID
         }
-        const result = await versionsService.create(newData);
-        return res.status(201).json(result);
+        const version = await versionsService.create(newData);
+        const result = {
+            meta: {
+                url: req.originalUrl
+            }
+        };
+        if(version){
+            result.data = {
+                version
+            }
+            result.meta.status = 201;
+            result.meta.count = 1;
+        }
+        else{
+            result.meta.status = 409;
+            result.meta.count = 0;
+            result.error= {
+                status: "409",
+                message: `No versions were found`
+            }
+        }
+        return res.status(result.meta.status).json(result);
     },
     update: async (req, res) => {
         const newData = {};
@@ -38,8 +137,28 @@ const versionsController = {
         if(req.body.brandID !== undefined){
             newData.brandID = req.body.brandID;
         }
-        const result = await versionsService.update(req.params.versionID, newData);
-        return res.status(202).json(result);
+        const version = await versionsService.update(req.params.versionID, newData);
+        const result = {
+            meta: {
+                url: req.originalUrl
+            }
+        };
+        if(version){
+            result.data = {
+                version
+            }
+            result.meta.status = 201;
+            result.meta.count = 1;
+        }
+        else{
+            result.meta.status = 409;
+            result.meta.count = 0;
+            result.error= {
+                status: "409",
+                message: `No versions were found`
+            }
+        }
+        return res.status(result.meta.status).json(result);
     },
     delete: async (req, res) => {
         const result = await versionsService.delete(req.params.versionID, req.params.confirm);
