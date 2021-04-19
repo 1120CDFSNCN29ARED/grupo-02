910b7ae51c37bef3385e49f708de4219188b2731
Santiago Maldini <santimaldini@gmail.com>
2021-04-19 01:15:56 -0300

    updated posts find all and created images service

--- website/controllers/api/postsController.js
+++ website/controllers/api/postsController.js
@@ -10,19 +10,87 @@ const postsService = require("../../services/postsService");
 const productsService = require("../../services/productsService");
 const partsService = require("../../services/partsService");
 const vehiclesService = require("../../services/vehiclesService");
+const questionsService = require("../../services/questionsService");
+const imagesService = require("../../services/imagesService");
 
 const postsController = {
     all: async (req, res) => {
         const posts = await postsService.findAll();
+        
         const result = {
             meta: {
                 url: req.originalUrl
             }
         };
         if(posts){
-            result.data = {
-                posts
-            }
+            result.data = [];
+            for(post of posts) {
+                const locality = await localitiesService.findByPk(post.locationID);
+                const province = await provincesService.findByPk(locality.provinceID);
+                const product = await productsService.findByPk(post.productID);
+                const brand = await brandsService.findByPk(product.brandID);
+                const model = await modelsService.findByPk(product.modelID);
+                const questions = await questionsService.findByPostID(post.postID);
+                const images = await imagesService.findByPostID(post.postID);
+                const seller = await usersService.findByPk(post.sellerID);
+                let part = null;
+                let vehicle = null;
+                let version = null;
+                const postData = {
+                    post: {
+                        title: post.title,
+                        description: post.description,
+                        published: post.published,
+                        publishedDate: post.publishedDate,
+                        onSale: post.onSale,
+                        price: post.price,
+                        discount: post.discount,
+                        stock: post.stock,
+                        rating: post.rating,
+                        state: post.state,
+                        featured: post.featured,
+                        brandID: brand.brandID,
+                        brandName: brand.brandName,
+                        modelID: model.modelID,
+                        modelName: model.modelName,
+                        active: post.active,
+                    },
+                    seller: {
+                        sellerID: seller.sellerID,
+                        userName: seller.userName,
+                        firstName: seller.firstName,
+                        lastName: seller.lastName,
+                    },
+                    location: {
+                        locationID: locality.localityID,
+                        localityName: locality.localityName,
+                        provinceID: province.provinceID,
+                        provinceName: province.provinceName,
+                    },
+                    questions,
+                    images,
+                }
+                if(product.productType === "vehicle"){
+                    vehicle = await vehiclesService.findByPk(product.vehicleID);
+                    version = await versionsService.findByPk(vehicle.versionID);
+                    postData.post.versionID = version.versionID;
+                    postData.post.versionName = version.versionName;
+                    postData.post.gearType = version.gearType;
+                    postData.post.year = version.year;
+                    postData.post.kilometers = version.kilometers;
+                    postData.post.color = version.color;
+                }
+                else if(product.productType === "part"){
+                    part = await partsService.findByPk(product.partID);
+                    postData.post.versionID = version.versionID;
+                    postData.post.partSerialNumber = version.partSerialNumber;
+                    postData.post.car = version.car;
+                    postData.post.motorcycle = version.motorcycle;
+                    postData.post.pickup = version.pickup;
+                    postData.post.truck = version.truck;
+                }
+                result.data.push(postData);
+            };            
             result.meta.status = 200;
             result.meta.count = posts.length;
         }
