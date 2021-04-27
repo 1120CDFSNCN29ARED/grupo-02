7e54ee9cd3835b7e5abac6bbf120d46fe2cc2674
Simon Huxter <simon.huxter@gmail.com>
2021-04-26 22:14:53 -0300

    Fix error on loading home that called getFullUser middleware

--- website/middlewares/userLoggedMiddleware.js
+++ website/middlewares/userLoggedMiddleware.js
@@ -1,11 +1,13 @@
-const User = require('../models/User');
+
+const usersService = require('../services/usersService');
 
 function userLoggedMiddleware(req, res, next) {  
   res.locals.isLogged = false;
   
   let userEmailInCookie = req.cookies.userEmail;
-  let userFromCookie = User.findUserByField("email", userEmailInCookie);
-  
+  //let userFromCookie = User.findUserByField("email", userEmailInCookie); //Fue la forma con models de hacer la llamada.
+  let userFromCookie = usersService.findOne(userEmailInCookie).datavalues;
+  //let userFromCookie = getFullUser(userEmailInCookie); //=> No funcion aporque llamo este middleware en app.use!!!
   if (userFromCookie) {
     req.session.assertUserLogged = userFromCookie;
   }
