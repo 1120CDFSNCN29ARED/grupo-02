02a773361b80f12511dfa9e7154dd27a16d2938c
Simon Huxter <simon.huxter@gmail.com>
2021-05-06 19:42:55 -0300

    Add reg Vlaidation and userForm Modifications

--- website/public/js/registrationValidation.js
+++ website/public/js/registrationValidation.js
@@ -31,6 +31,8 @@ window.addEventListener("load", function () {
 	let confirmPasswordErr = document.getElementById("confirmPasswordErr");
 	let imageErr = document.getElementById("imageErr");
 
+	//passwordStrength Field
+	let passwordStrength = document.getElementById('passwordStrength');
 	//Validation Rules
 	userName.addEventListener("input", async (e) => {
 		/* userName 
@@ -160,14 +162,33 @@ window.addEventListener("load", function () {
     length>2
   */
     if (firstName.value) {
-      let firstNameToTest = firstName.value.trim();
+      firstNameErr.classList.contains("d-none")
+				? "" : firstNameErr.classList.add("d-none");
+			firstNameErr.innerText = "";
+			firstName.classList.contains("is-invalid")
+				? firstName.classList.remove("is-invalid"):"";
+			
+			let firstNameToTest = firstName.value.trim();
+			let valLength = validLength(firstNameToTest, 2);
+			
+			if (!valLength) {
+				firstNameErr.classList.contains("d-none")
+					? firstNameErr.classList.remove("d-none")
+					: "";
 
+				firstName.classList.contains("is-invalid")
+					? ""
+					: firstName.classList.add("is-invalid");
+
+				firstNameErr.innerText =
+					"El nombre deberia contener al menos 2 caracteres";	
+			}
     } else {
       firstNameErr.classList.contains("d-none")
 				? firstNameErr.classList.remove("d-none")
 				: "";
 
-			userName.classList.contains("is-invalid")
+			firstName.classList.contains("is-invalid")
 				? ""
 				: firstName.classList.add("is-invalid");
 
@@ -175,26 +196,205 @@ window.addEventListener("load", function () {
 				"El nombre deberia contener al menos 2 caracteres";
     }
 	});
-});
-
-//Validation Rules
-
-/* lastName
+	lastName.addEventListener("input", async (e) => {
+		/* lastName 
     required
     length>2
   */
-/* dni 
+		if (lastName.value) {
+			lastNameErr.classList.contains("d-none")
+				? ""
+				: lastNameErr.classList.add("d-none");
+			lastNameErr.innerText = "";
+			lastName.classList.contains("is-invalid")
+				? lastName.classList.remove("is-invalid")
+				: "";
+
+			let firstNameToTest = lastName.value.trim();
+			let valLength = validLength(firstNameToTest, 2);
+
+			if (!valLength) {
+				lastNameErr.classList.contains("d-none")
+					? lastNameErr.classList.remove("d-none")
+					: "";
+
+				lastName.classList.contains("is-invalid")
+					? ""
+					: lastName.classList.add("is-invalid");
+
+				lastNameErr.innerText =
+					"El apellido deberia contener al menos 2 caracteres";
+			}
+		} else {
+			lastNameErr.classList.contains("d-none")
+				? lastNameErr.classList.remove("d-none")
+				: "";
+
+			lastName.classList.contains("is-invalid")
+				? ""
+				: lastName.classList.add("is-invalid");
+
+			lastNameErr.innerText =
+				"El apellido deberia contener al menos 2 caracteres";
+		}
+	});
+
+	dni.addEventListener('input', (e) => {
+		/* dni 
     required
     valid Arg ID format
       length: up to 8 characters
       only number NO "." or "-"
   */
-/* telephone 
+		if (dni.value) {
+			dniErr.classList.contains("d-none") ? "" : dniErr.classList.add("d-none");
+			dniErr.innerText = "";
+			dni.classList.contains("is-invalid")
+				? dni.classList.remove("is-invalid")
+				: "";
+
+			let dniToTest = dni.value.trim();
+			let validID = isValidID(dniToTest);
+			console.log("Id is Valid: ", validID);
+			if (validID) {
+				dni.value = formatID(dniToTest);
+			} else {
+				dniErr.classList.contains("d-none")
+					? dniErr.classList.remove("d-none")
+					: "";
+
+				dni.classList.contains("is-invalid")
+					? ""
+					: dni.classList.add("is-invalid");
+
+				dniErr.innerText = "Debe contener 8 characteres";
+			}
+		} else {
+			dniErr.classList.contains("d-none")
+				? dniErr.classList.remove("d-none")
+				: "";
+
+			dni.classList.contains("is-invalid")
+				? ""
+				: dni.classList.add("is-invalid");
+
+			dniErr.innerText = "Debe contener 8 characteres";
+		}
+	})
+
+	telephone.addEventListener('input', (e) => {
+		/* telephone 
     required  
     valid Arg number
-      length
-      only numbers
+      length: 11-13 digits
+      only numbers 
   */
+		if (telephone.value) {
+			telephoneErr.classList.contains("d-none") ? "" : telephoneErr.classList.add("d-none");
+			telephoneErr.innerText = "";
+			telephone.classList.contains("is-invalid")
+				? telephone.classList.remove("is-invalid")
+				: "";
+			let telephoneToTest = telephone.value
+			let valTelephone = isTelephone(telephoneToTest);
+			
+			if (!valTelephone) {
+				telephoneErr.classList.contains("d-none")
+					? telephoneErr.classList.remove("d-none")
+					: "";
+
+				telephone.classList.contains("is-invalid")
+					? ""
+					: telephone.classList.add("is-invalid");
+
+				telephoneErr.innerText = "Por favor ingrese un teléfono valido";	
+			}
+		} else {
+			telephoneErr.classList.contains("d-none")
+				? telephoneErr.classList.remove("d-none")
+				: "";
+
+			telephone.classList.contains("is-invalid")
+				? ""
+				: telephone.classList.add("is-invalid");
+
+			telephoneErr.innerText = "Por favor ingrese un teléfono valido \n Celular: (xxx)-15-xxxx-xxxx \n Fijo: (xxx)-xxxx-xxxx";
+		}
+	});
+
+	postalCode.addEventListener('input', (e) => {
+		/* postalCode 
+    required
+    valid Arg postal code of 4 numbers
+    length 4
+    only numbers
+  */
+		if (postalCode.value) {
+			postalCodeErr.classList.contains("d-none")
+				? ""
+				: postalCodeErr.classList.add("d-none");
+			postalCodeErr.innerText = "";
+			postalCode.classList.contains("is-invalid")
+				? postalCode.classList.remove("is-invalid")
+				: "";
+			let pcToTest = postalCode.value;
+			let validPC = isPostalCode(pcToTest);
+			if (!validPC) {
+				postalCodeErr.classList.contains("d-none")
+					? postalCodeErr.classList.remove("d-none")
+					: "";
+
+				postalCode.classList.contains("is-invalid")
+					? ""
+					: postalCode.classList.add("is-invalid");
+
+				postalCodeErr.innerText =
+					"Por favor ingrese un código postal válido de 4 digitos";	
+			}
+			
+		} else {
+			postalCodeErr.classList.contains("d-none")
+				? postalCodeErr.classList.remove("d-none")
+				: "";
+
+			postalCode.classList.contains("is-invalid")
+				? ""
+				: postalCode.classList.add("is-invalid");
+
+			postalCodeErr.innerText =
+				"Por favor ingrese un código postal válido de 4 digitos";
+		}
+	});
+
+	password.addEventListener('input', (e) => {
+		if (password.value) {
+			console.log("The password is being generated");
+			let passwordToTest = password.value;
+			let validityScore = validPassword(passwordToTest);
+			passwordStrength.classList.remove('d-none');
+			passwordStrength.innerText="Pswd Strength: "+ validityScore;
+			console.log(validityScore);
+			switch (validityScore) {
+				
+			}
+		} else {
+			passwordErr.classList.contains("d-none")
+				? passwordErr.classList.remove("d-none")
+				: "";
+
+			password.classList.contains("is-invalid")
+				? ""
+				: password.classList.add("is-invalid");
+
+			passwordErr.innerText =
+				"Por favor ingrese una contraseña válida con más de 8 caracteres y : \n Mayusucla \n Miniscula \n Numero \ Caracter Especial";
+		}
+	});
+});
+
+//Validation Rules
+
+
 /* province 
     required
     Selected form pre-populated list
@@ -203,12 +403,7 @@ window.addEventListener("load", function () {
     required
     selcted form pre-populated list based on province
   */
-/* postalCode 
-    required
-    valid Arg postal code of 4 numbers
-    length 4
-    only numbers
-  */
+
 /* address 
     required
     How to Validate?
@@ -234,12 +429,12 @@ window.addEventListener("load", function () {
 /* 
 notEmpty=>test fo rvalue
 required => if value does not exist then error
-alpha =>
-alpha-numeric =>creaed regex function
+alpha => Created regex function
+alpha-numeric =>created regex function
 numeric => create function to check if integer
 length => created function
-id number
-telephone
+id number => create regex function
+telephone => create regex function
 Password with REGEX
 */
 
@@ -265,4 +460,39 @@ function isAlphaNumeric(field) {
 function isAlpha(field) {
 	const re = /^[a-zñáéíóúü]+$/i;
 	return re.test(String(field).toLowerCase);
+}
+
+function isPostalCode(field) {
+	const re = /^(\d{4})$/ig;
+	return re.test(String(field));
+}
+
+function isValidID(idNumber) {
+	const re = /^((\d{2}\.{1}\d{3}\.\d{3})|(\d{2}\s{1}\d{3}\s\d{3})|(\d{8}))$/g
+	return re.test(idNumber);
+}
+
+function formatID(validID) {
+	const re = /\s|\.+/g
+	return validID.replace(re, '');
+}
+
+function isTelephone(telephone) {
+	const re = /^(?=(((\D*\d){11})|((\D*\d){13}))$)\(?\d{3,5}\)?[- .]?\d{2}[- .]?\d{2,4}[- .]?\d{4}$/g;
+	return re.test(telephone);
+}
+
+function validPassword(passwordToTest) {
+	let re = [];
+	re.push("[A-Z]"); //UpperCase
+	re.push("[a-z]"); //LowerCase
+	re.push("[0-9]"); //Digit
+	re.push("[$@$!%*#?&]"); //Special Characters
+	let passed = 0;
+	for (let i = 0; i < re.length; i++){
+		if (new RegExp(re[i]).test(String(passwordToTest))) {
+			passed++;
+		}
+	}
+	return passed;
 }
