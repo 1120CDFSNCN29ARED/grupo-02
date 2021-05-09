212e0255559862f4cfee8d7b9225bc9b94ce33a8
Simon Huxter <simon.huxter@gmail.com>
2021-05-09 18:52:51 -0300

    Implement confirmPAssword Validation on registration form

--- website/public/js/registrationValidation.js
+++ website/public/js/registrationValidation.js
@@ -1,7 +1,34 @@
-
 window.addEventListener("load", function () {
 	let MYPORT = 3000;
-	const baseURL = `http://localhost:${MYPORT}/api`;
+	const baseUrl = `http://localhost:${MYPORT}/api`;
+	
+	const divGenerator = (msg) => {
+		const div = document.createElement("div");
+		div.classList.add("invalid-feedback");
+		div.innerText = msg;
+		return div;
+	};
+
+	const divChecker = (element, condition, msg) => {
+		if (condition) {
+			element.classList.add("is-invalid");
+			if (
+				element.nextElementSibling &&
+				element.nextElementSibling.tagName !== "DIV"
+			) {
+				element.insertAdjacentElement("afterend", divGenerator(msg));
+			}
+		} else {
+			element.classList.remove("is-invalid");
+			if (
+				element.nextElementSibling &&
+				element.nextElementSibling.tagName === "DIV"
+			) {
+				element.nextElementSibling.remove();
+			}
+		}
+	};
+
 	//Fields
 	let userName = document.getElementById("userName");
 	let email = document.getElementById("email");
@@ -32,15 +59,9 @@ window.addEventListener("load", function () {
 	let imageErr = document.getElementById("imageErr");
 
 	//passwordStrength Field
-	let passwordStrength = document.getElementById('passwordStrength');
+	let passwordStrength = document.getElementById("passwordStrength");
 	//Validation Rules
 	userName.addEventListener("input", async (e) => {
-		/* userName 
-    required
-    length>4
-    cannot be already be in use -> API
-    */
-
 		if (userName.value) {
 			let userNameToTest = userName.value.trim();
 			let valLength = validLength(userNameToTest, 4);
@@ -49,87 +70,84 @@ window.addEventListener("load", function () {
 					? ""
 					: userNameErr.classList.add("d-none");
 				userNameErr.innerText = "";
-				
-        userName.classList.contains("is-invalid")
+
+				userName.classList.contains("is-invalid")
 					? userName.classList.remove("is-invalid")
 					: "";
-				
-        let res = await fetch(`${baseURL}/users/byUserName/${userNameToTest}`);
+
+				let res = await fetch(`${baseURL}/users/byUserName/${userNameToTest}`);
 				let user = await res.json();
 				console.log("This is the user: ", user);
-				
-        if (!user.error) {
+
+				if (!user.error) {
 					console.warn('The username is already in use");');
-					
-          userNameErr.classList.contains("d-none")
+
+					userNameErr.classList.contains("d-none")
 						? userNameErr.classList.remove("d-none")
 						: "";
-					
-          userName.classList.contains("is-invalid")
+
+					userName.classList.contains("is-invalid")
 						? ""
 						: userName.classList.add("is-invalid");
-					
-          userNameErr.innerText = "Por favor elige otro nombre de usuario.";
+
+					userNameErr.innerText = "Por favor elige otro nombre de usuario.";
 				}
 			} else {
 				console.log("the username is too short");
-				
-        userNameErr.classList.contains("d-none")
+
+				userNameErr.classList.contains("d-none")
 					? userNameErr.classList.remove("d-none")
 					: "";
-				
-        userNameErr.innerText =
+
+				userNameErr.innerText =
 					"El nombre del Usuario deberia contener al menos 4 caracteres";
-				
-        userName.classList.contains("is-invalid")
+
+				userName.classList.contains("is-invalid")
 					? ""
 					: userName.classList.add("is-invalid");
 			}
 		} else {
 			console.log("El nombre de usuario es invalido");
-			
-      userNameErr.classList.contains("d-none")
+
+			userNameErr.classList.contains("d-none")
 				? userNameErr.classList.remove("d-none")
 				: "";
-			
-      userName.classList.contains("is-invalid")
+
+			userName.classList.contains("is-invalid")
 				? ""
 				: userName.classList.add("is-invalid");
-			
-      userNameErr.innerText =
+
+			userNameErr.innerText =
 				"El nombre del Usuario deberia contener al menos 4 caracteres";
 		}
 	});
 	//EMAIL
 	email.addEventListener("input", async (e) => {
-		/* email 
-    required
-    valid email format
-    cannot be in use -> API
-  */
-    if (email.value) {
-      emailErr.classList.contains("d-none")
-				? "":emailErr.classList.add("d-none");
+		if (email.value) {
+			emailErr.classList.contains("d-none")
+				? ""
+				: emailErr.classList.add("d-none");
 			emailErr.innerText = "";
 			email.classList.contains("is-invalid")
-        ? email.classList.remove("is-invalid") : "";
-      
-      let emailToTest = email.value.trim();
-      let validEmail = validateEmail(emailToTest);
-      
-      if (!validEmail) {
-        emailErr.classList.contains("d-none")
+				? email.classList.remove("is-invalid")
+				: "";
+
+			let emailToTest = email.value.trim();
+			let validEmail = validateEmail(emailToTest);
+
+			if (!validEmail) {
+				emailErr.classList.contains("d-none")
 					? emailErr.classList.remove("d-none")
-          : "";
-        emailErr.innerText="El mail ingresado no es válido"
-        email.classList.contains("is-invalid")
+					: "";
+				emailErr.innerText = "El mail ingresado no es válido";
+				email.classList.contains("is-invalid")
 					? ""
 					: email.classList.add("is-invalid");
-      } else {
-        let res = await fetch(`${baseURL}/users/byEmail/${emailToTest}`);
-        let user = await res.json();
-        
-        if (!user.error) {
+			} else {
+				let res = await fetch(`${baseURL}/users/byEmail/${emailToTest}`);
+				let user = await res.json();
+
+				if (!user.error) {
 					emailErr.classList.contains("d-none")
 						? emailErr.classList.remove("d-none")
 						: "";
@@ -137,40 +155,41 @@ window.addEventListener("load", function () {
 					email.classList.contains("is-invalid")
 						? ""
 						: email.classList.add("is-invalid");
-        } else {
-          emailErr.classList.contains("d-none")
+				} else {
+					emailErr.classList.contains("d-none")
 						? ""
 						: emailErr.classList.add("d-none");
 					emailErr.innerText = "";
 					email.classList.contains("in-valid")
 						? email.classList.remove("in-valid")
 						: "";
-        }
-      }
+				}
+			}
 		} else {
-		emailErr.classList.contains("d-none")
-			? emailErr.classList.remove("d-none")
-			: "";
-		emailErr.innerText = "El mail ingresado no es válido";
-		email.classList.contains("in-valid") ? "" : email.classList.add("in-valid");
-    }
-  });
-  
-  firstName.addEventListener('input', async (e) => {
-		/* firstName 
-    required
-    length>2
-  */
-    if (firstName.value) {
-      firstNameErr.classList.contains("d-none")
-				? "" : firstNameErr.classList.add("d-none");
+			emailErr.classList.contains("d-none")
+				? emailErr.classList.remove("d-none")
+				: "";
+			emailErr.innerText = "El mail ingresado no es válido";
+			email.classList.contains("in-valid")
+				? ""
+				: email.classList.add("in-valid");
+		}
+	});
+
+	/* Firstname */
+	firstName.addEventListener("input", async (e) => {
+		if (firstName.value) {
+			firstNameErr.classList.contains("d-none")
+				? ""
+				: firstNameErr.classList.add("d-none");
 			firstNameErr.innerText = "";
 			firstName.classList.contains("is-invalid")
-				? firstName.classList.remove("is-invalid"):"";
-			
+				? firstName.classList.remove("is-invalid")
+				: "";
+
 			let firstNameToTest = firstName.value.trim();
 			let valLength = validLength(firstNameToTest, 2);
-			
+
 			if (!valLength) {
 				firstNameErr.classList.contains("d-none")
 					? firstNameErr.classList.remove("d-none")
@@ -181,10 +200,10 @@ window.addEventListener("load", function () {
 					: firstName.classList.add("is-invalid");
 
 				firstNameErr.innerText =
-					"El nombre deberia contener al menos 2 caracteres";	
+					"El nombre deberia contener al menos 2 caracteres";
 			}
-    } else {
-      firstNameErr.classList.contains("d-none")
+		} else {
+			firstNameErr.classList.contains("d-none")
 				? firstNameErr.classList.remove("d-none")
 				: "";
 
@@ -194,13 +213,11 @@ window.addEventListener("load", function () {
 
 			firstNameErr.innerText =
 				"El nombre deberia contener al menos 2 caracteres";
-    }
+		}
 	});
+
+	/* LastName */
 	lastName.addEventListener("input", async (e) => {
-		/* lastName 
-    required
-    length>2
-  */
 		if (lastName.value) {
 			lastNameErr.classList.contains("d-none")
 				? ""
@@ -239,13 +256,8 @@ window.addEventListener("load", function () {
 		}
 	});
 
-	dni.addEventListener('input', (e) => {
-		/* dni 
-    required
-    valid Arg ID format
-      length: up to 8 characters
-      only number NO "." or "-"
-  */
+	/* DNI */
+	dni.addEventListener("input", (e) => {
 		if (dni.value) {
 			dniErr.classList.contains("d-none") ? "" : dniErr.classList.add("d-none");
 			dniErr.innerText = "";
@@ -280,24 +292,21 @@ window.addEventListener("load", function () {
 
 			dniErr.innerText = "Debe contener 8 characteres";
 		}
-	})
+	});
 
-	telephone.addEventListener('input', (e) => {
-		/* telephone 
-    required  
-    valid Arg number
-      length: 11-13 digits
-      only numbers 
-  */
+	/* Telephone */
+	telephone.addEventListener("input", (e) => {
 		if (telephone.value) {
-			telephoneErr.classList.contains("d-none") ? "" : telephoneErr.classList.add("d-none");
+			telephoneErr.classList.contains("d-none")
+				? ""
+				: telephoneErr.classList.add("d-none");
 			telephoneErr.innerText = "";
 			telephone.classList.contains("is-invalid")
 				? telephone.classList.remove("is-invalid")
 				: "";
-			let telephoneToTest = telephone.value
+			let telephoneToTest = telephone.value;
 			let valTelephone = isTelephone(telephoneToTest);
-			
+
 			if (!valTelephone) {
 				telephoneErr.classList.contains("d-none")
 					? telephoneErr.classList.remove("d-none")
@@ -307,7 +316,7 @@ window.addEventListener("load", function () {
 					? ""
 					: telephone.classList.add("is-invalid");
 
-				telephoneErr.innerText = "Por favor ingrese un teléfono valido";	
+				telephoneErr.innerText = "Por favor ingrese un teléfono valido";
 			}
 		} else {
 			telephoneErr.classList.contains("d-none")
@@ -318,17 +327,19 @@ window.addEventListener("load", function () {
 				? ""
 				: telephone.classList.add("is-invalid");
 
-			telephoneErr.innerText = "Por favor ingrese un teléfono valido \n Celular: (xxx)-15-xxxx-xxxx \n Fijo: (xxx)-xxxx-xxxx";
+			telephoneErr.innerText =
+				"Por favor ingrese un teléfono valido \n Celular: (xxx)-15-xxxx-xxxx \n Fijo: (xxx)-xxxx-xxxx";
 		}
 	});
 
-	postalCode.addEventListener('input', (e) => {
-		/* postalCode 
-    required
-    valid Arg postal code of 4 numbers
-    length 4
-    only numbers
-  */
+	/* Province */
+	province.addEventListener("change", async (e) => {
+		await provinceValidation();
+	});
+	/* Locality */
+
+	/* PostalCode */
+	postalCode.addEventListener("input", (e) => {
 		if (postalCode.value) {
 			postalCodeErr.classList.contains("d-none")
 				? ""
@@ -349,9 +360,8 @@ window.addEventListener("load", function () {
 					: postalCode.classList.add("is-invalid");
 
 				postalCodeErr.innerText =
-					"Por favor ingrese un código postal válido de 4 digitos";	
+					"Por favor ingrese un código postal válido de 4 digitos";
 			}
-			
 		} else {
 			postalCodeErr.classList.contains("d-none")
 				? postalCodeErr.classList.remove("d-none")
@@ -366,7 +376,8 @@ window.addEventListener("load", function () {
 		}
 	});
 
-	password.addEventListener('input', (e) => {
+	/* Password */
+	password.addEventListener("input", (e) => {
 		passwordErr.classList.contains("d-none")
 			? ""
 			: passwordErr.classList.add("d-none");
@@ -378,7 +389,7 @@ window.addEventListener("load", function () {
 			console.log("The password is being generated");
 			let passwordToTest = password.value;
 			let validityScore = validPassword(passwordToTest);
-			passwordStrength.classList.remove('d-none');
+			passwordStrength.classList.remove("d-none");
 
 			console.log(validityScore);
 			let strength = "";
@@ -391,18 +402,17 @@ window.addEventListener("load", function () {
 					passwordErr.classList.contains("d-none")
 						? passwordErr.classList.remove("d-none")
 						: "";
-					passwordErr.innerHTML =
-						validityScore.errorMsg;
+					passwordErr.innerHTML = validityScore.errorMsg;
 
 					password.classList.contains("is-invalid")
 						? ""
 						: password.classList.add("is-invalid");
 
-					
 					break;
 				case 3:
 				case 4:
-					strength = "<small class='progress-bar bg-warning' style='width: 60%'>Mediano</small>";
+					strength =
+						"<small class='progress-bar bg-warning' style='width: 60%'>Mediano</small>";
 					passwordErr.classList.contains("d-none")
 						? passwordErr.classList.remove("d-none")
 						: "";
@@ -412,11 +422,12 @@ window.addEventListener("load", function () {
 						? ""
 						: password.classList.add("is-invalid");
 
-          break;
-        case 5:
-					strength = "<small class='progress-bar bg-success' style='width: 100%'>Fuerte</small>";
+					break;
+				case 5:
+					strength =
+						"<small class='progress-bar bg-success' style='width: 100%'>Fuerte</small>";
 					password.classList.add("is-valid");
-          break;
+					break;
 			}
 			passwordStrength.innerHTML = strength;
 		} else {
@@ -430,10 +441,53 @@ window.addEventListener("load", function () {
 				: password.classList.add("is-invalid");
 		}
 	});
+
+	/* ConfirmPassword */
+	confirmPassword.addEventListener("change", (e) => {
+		confirmPasswordErr.classList.contains("d-none")
+			? ""
+			: confirmPasswordErr.classList.add("d-none");
+		confirmPassword.classList.contains("is-invalid")
+			? confirmPassword.classList.remove("is-invalid")
+			: "";
+
+		let pswdChecker = false;
+		console.log(confirmPassword.value);
+		console.log("The password is valid: ", password.value);
+		if (confirmPassword.value && validPassword(password.value).passed == 5) {
+			pswdChecker = isMatching(confirmPassword.value, password.value);
+			console.log("The passwords match: ", pswdChecker);
+			if (pswdChecker) {
+				confirmPasswordErr.classList.contains("d-none")
+					? ""
+					: confirmPasswordErr.classList.add("d-none");
+				confirmPassword.classList.contains("is-invalid")
+					? confirmPassword.classList.remove("is-invalid")
+					: "";
+				confirmPassword.classList.add("is-valid");
+			} else {
+				confirmPasswordErr.classList.contains("d-none")
+					? confirmPasswordErr.classList.remove("d-none")
+					: "";
+				confirmPasswordErr.innerText =
+					"Por favor reingrese la misma contraseña";
+				confirmPassword.classList.contains("is-invalid")
+					? ""
+					: confirmPassword.classList.add("is-invalid");
+			}
+		} else {
+			confirmPasswordErr.classList.contains("d-none")
+				? confirmPasswordErr.classList.remove("d-none")
+				: "";
+			confirmPasswordErr.innerText = "Por favor reingrese la misma contraseña";
+			confirmPassword.classList.contains("is-invalid")
+				? ""
+				: confirmPassword.classList.add("is-invalid");
+		}
+	});
 });
 
 //Validation Rules
-
 
 /* province 
     required
@@ -448,35 +502,11 @@ window.addEventListener("load", function () {
     required
     How to Validate?
   */
-/* password 
-    required
-    length>8
-    one Cpaital
-    one lower
-    one number
-    one special character
-  */
-/* confirmPassword 
-    required
-    Coincide with password
-  */
+
 /* image 
     Optional
     Valid formats and size limits
   */
-
-//Validation Functions
-/* 
-notEmpty=>test fo rvalue
-required => if value does not exist then error
-alpha => Created regex function
-alpha-numeric =>created regex function
-numeric => create function to check if integer
-length => created function
-id number => create regex function
-telephone => create regex function
-Password with REGEX
-*/
 
 function validLength(testValue, rule) {
 	let validLength = false;
@@ -488,13 +518,13 @@ function validLength(testValue, rule) {
 
 function validateEmail(email) {
 	//const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  const re = /\w[-\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,14}/;
-  return re.test(String(email).toLowerCase());
+	const re = /\w[-\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\.)+[A-Za-z]{2,14}/;
+	return re.test(String(email).toLowerCase());
 }
 
 function isAlphaNumeric(field) {
-  const re = /^[0-9a-zñáéíóúü]+$/i;
-  return re.test(String(field).toLowerCase);
+	const re = /^[0-9a-zñáéíóúü]+$/i;
+	return re.test(String(field).toLowerCase);
 }
 
 function isAlpha(field) {
@@ -503,18 +533,18 @@ function isAlpha(field) {
 }
 
 function isPostalCode(field) {
-	const re = /^(\d{4})$/ig;
+	const re = /^(\d{4})$/gi;
 	return re.test(String(field));
 }
 
 function isValidID(idNumber) {
-	const re = /^((\d{2}\.{1}\d{3}\.\d{3})|(\d{2}\s{1}\d{3}\s\d{3})|(\d{8}))$/g
+	const re = /^((\d{2}\.{1}\d{3}\.\d{3})|(\d{2}\s{1}\d{3}\s\d{3})|(\d{8}))$/g;
 	return re.test(idNumber);
 }
 
 function formatID(validID) {
-	const re = /\s|\.+/g
-	return validID.replace(re, '');
+	const re = /\s|\.+/g;
+	return validID.replace(re, "");
 }
 
 function isTelephone(telephone) {
@@ -556,9 +586,15 @@ function validPassword(passwordToTest) {
 	re.push("([A-Za-z0-9$@$!%*#?&]){8,}"); //length */
 	let passed = 0;
 	let errorMsg = "";
-	Object.values(re).forEach(condition => {
-
-		console.log("TESTING ",condition ," - ", condition.condition,": ",new RegExp(condition.condition).test(passwordToTest));
+	Object.values(re).forEach((condition) => {
+		console.log(
+			"TESTING ",
+			condition,
+			" - ",
+			condition.condition,
+			": ",
+			new RegExp(condition.condition).test(passwordToTest)
+		);
 		if (new RegExp(condition.condition).test(passwordToTest)) {
 			passed++;
 			console.log(passed);
@@ -574,3 +610,40 @@ function validPassword(passwordToTest) {
 	} */
 	return { passed, errorMsg };
 }
+
+function isMatching(string1, string2) {
+	isMatch = false;
+	string1 === string2 ? (isMatch = true) : "";
+	return isMatch;
+}
+/* 
+async function provinceValidation() {
+	if (province.value) {
+		let res = await fetch(`${baseURL}/provinces/id/${province.value}`);
+		let provinceTest = await res.json();
+		divChecker(
+			province,
+			provinceTest.error,
+			"La provincia elegida es inválida"
+		);
+	} else {
+		province.classList.add("is-invalid");
+		province.insertAdjacentElement(
+			"afterend",
+			divGenerator("Debe elegir una provincia")
+		);
+	}
+}
+async function locationValidation() {
+	if (location.value) {
+		let res = await fetch(`${baseURL}/localities/id/${location.value}`);
+		let locationTest = await res.json();
+		divChecker(location, locationTest.error, "La ciudad elegida es inválida");
+	} else {
+		location.classList.add("is-invalid");
+		location.insertAdjacentElement(
+			"afterend",
+			divGenerator("Debe elegir una ciudad")
+		);
+	}
+} */
