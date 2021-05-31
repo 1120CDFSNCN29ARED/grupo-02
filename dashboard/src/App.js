7296992d424c5f8a7d2eaf717caf7c69883578f1
Santiago Maldini <santimaldini@gmail.com>
2021-05-31 14:28:29 -0300

    initial small card and graph version

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LineGraph from "./components/AreaGraph/AreaGraph";
+import SmallCard from "./components/SmallCard/SmallCard";
 
 function App() {
+  const chart = {
+    width: 250,
+    height: 150,
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SmallCard
+        backgroundColor="red"
+        title="Usuarios"
+        total="123"
+        chart={chart}
+      />
+      <LineGraph />
     </div>
   );
 }
---
+++ dashboard/src/components/SmallCard/SmallCard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import AreaGraph from "../AreaGraph/AreaGraph";
+
+function SmallCard({ title, total, backgroundColor, chart }) {
+  const useStyles = makeStyles({
+    root: {
+      width: 275,
+      padding: 0,
+      backgroundColor: backgroundColor,
+      color: "white",
+      textAlign: "left",
+      justifyContent: "left",
+    },
+    total: { fontSize: "1.3rem" },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+  const classes = useStyles();
+  return (
+    <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography
+          variant="h5"
+          component="h2"
+          className={classes.total}
+          gutterBottom
+        >
+          {total}
+        </Typography>
+        <Typography className={classes.pos}>{title}</Typography>
+        <AreaGraph width={chart.width} height={chart.height} />
+      </CardContent>
+    </Card>
+  );
+}
+
+export default SmallCard;
